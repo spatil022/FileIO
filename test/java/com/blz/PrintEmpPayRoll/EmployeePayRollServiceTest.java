ce71917c1c05ab4273bd58a63ced6f9caeb06b77
spatil022 <shamalpatil1998@gmail.com>
2021-03-07 23:11:04 +0530

    added

---
+++ test/java/com/blz/PrintEmpPayRoll/EmployeePayRollServiceTest.java
@@ -0,0 +1,30 @@
+package com.blz.PrintEmpPayRoll;
+
+import java.util.Arrays;
+import org.junit.BeforeClass;
+import org.junit.Test;
+public class EmployeePayRollServiceTest 
+{
+	static EmployeePayrollService employeePayrollService;
+	@BeforeClass
+	public static void initializeConstructor()
+	{
+		employeePayrollService = new EmployeePayrollService();
+	}
+	@Test
+	public void printWelcomeMessage() {
+		employeePayrollService.printWelcomeMessage();
+	}
+	
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
+		EmployeePayrollData[] arrayOfEmps = {
+				new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+				new EmployeePayrollData(2, "Bill Gates", 200000.0),
+				new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0)
+		};
+		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+		employeePayrollService.printData(EmployeePayrollService.IOService.FILE_IO);
+	}	
+} 
